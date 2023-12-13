7f0f801f2a624a4ff46ee92a585e34aa298de6b4
dimavoronkov2222@hotmail.com <dimavoronkov2222@hotmail.com>
2023-12-13 19:44:20 +0100

    2323

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,48 +38,80 @@ namespace Module_1_HW
 
         private void Loaddata(SqlConnection connection)
         {
-            SqlCommand command = new SqlCommand("SELECT Name FROM VegetablesAndFruits ORDER BY Name", connection);
+            // Display the count of vegetables
+            SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM VegetablesAndFruits WHERE Type = 'Vegetable'", connection);
             SqlDataReader reader = command.ExecuteReader();
-            // Display all the names of vegetables and fruits
             while (reader.Read())
             {
-                names.Add(reader[0].ToString());
+                names.Add("Count of vegetables: " + reader[0].ToString());
             }
             reader.Close();
 
-            command = new SqlCommand("SELECT Color FROM VegetablesAndFruits ORDER BY Color", connection);
+            // Display the count of fruits
+            command = new SqlCommand("SELECT COUNT(*) FROM VegetablesAndFruits WHERE Type = 'Fruit'", connection);
             reader = command.ExecuteReader();
-            // Display all the colors
             while (reader.Read())
             {
-                colors.Add(reader[0].ToString());
+                names.Add("Count of fruits: " + reader[0].ToString());
             }
             reader.Close();
 
-            command = new SqlCommand("SELECT MAX(Caloricity) FROM VegetablesAndFruits", connection);
+            // Display the count of vegetables and fruits of a given color
+            string color = "Red"; // replace with the color you want
+            command = new SqlCommand($"SELECT COUNT(*) FROM VegetablesAndFruits WHERE Color = '{color}'", connection);
             reader = command.ExecuteReader();
-            // Display the maximum calories
             while (reader.Read())
             {
-                caloricities.Add(reader[0].ToString());
+                colors.Add($"Count of vegetables and fruits of {color} color: " + reader[0].ToString());
             }
             reader.Close();
 
-            command = new SqlCommand("SELECT MIN(Caloricity) FROM VegetablesAndFruits", connection);
+            // Display the count of vegetables and fruits of each color
+            command = new SqlCommand("SELECT Color, COUNT(*) FROM VegetablesAndFruits GROUP BY Color", connection);
             reader = command.ExecuteReader();
-            // Display the minimum calories
             while (reader.Read())
             {
-                caloricities.Add(reader[0].ToString());
+                colors.Add($"Count of vegetables and fruits of {reader[0]} color: " + reader[1].ToString());
             }
             reader.Close();
 
-            command = new SqlCommand("SELECT AVG(Caloricity) FROM VegetablesAndFruits", connection);
+            // Display vegetables and fruits with caloricity below the specified
+            int maxCaloricity = 100; // replace with the caloricity you want
+            command = new SqlCommand($"SELECT Name FROM VegetablesAndFruits WHERE Caloricity < {maxCaloricity}", connection);
             reader = command.ExecuteReader();
-            // Display the average calories
             while (reader.Read())
             {
-                caloricities.Add(reader[0].ToString());
+                caloricities.Add($"Vegetables and fruits with caloricity below {maxCaloricity}: " + reader[0].ToString());
+            }
+            reader.Close();
+
+            // Display vegetables and fruits with caloricity above the specified
+            int minCaloricity = 50; // replace with the caloricity you want
+            command = new SqlCommand($"SELECT Name FROM VegetablesAndFruits WHERE Caloricity > {minCaloricity}", connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                caloricities.Add($"Vegetables and fruits with caloricity above {minCaloricity}: " + reader[0].ToString());
+            }
+            reader.Close();
+
+            // Display vegetables and fruits with caloricity in the specified range
+            int lowerBound = 50; // replace with the lower bound you want
+            int upperBound = 100; // replace with the upper bound you want
+            command = new SqlCommand($"SELECT Name FROM VegetablesAndFruits WHERE Caloricity BETWEEN {lowerBound} AND {upperBound}", connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                caloricities.Add($"Vegetables and fruits with caloricity between {lowerBound} and {upperBound}: " + reader[0].ToString());
+            }
+            reader.Close();
+
+            // Display all vegetables and fruits of yellow or red color
+            command = new SqlCommand("SELECT Name FROM VegetablesAndFruits WHERE Color IN ('Yellow', 'Red')", connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                colors.Add("Vegetables and fruits of yellow or red color: " + reader[0].ToString());
             }
             reader.Close();
         }
